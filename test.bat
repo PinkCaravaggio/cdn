4d9a74f7500d96fa1dee6835c25f8a6024796e11
Pink Caravaggio <letongchen510@gmail.com>
2025-11-12 10:33:29 -0800

    Auto update 12-11-2025 10:33

--- test.bat
+++ test.bat
@@ -1,23 +1,6 @@
-@echo off
-setlocal EnableDelayedExpansion
+echo %ESC%[31mThis text is red.%ESC%[0m
+echo %ESC%[32mThis text is green.%ESC%[0m
+echo %ESC%[1;34mThis text is bold blue.%ESC%[0m
+echo %ESC%[4mThis text is underlined.%ESC%[0m
 
-cd /d "%~dp0"
-
-set "BASE_URL=https://letongchen.art/cdn/images"
-set "IMAGE_DIR=images"
-echo.
-echo Uploaded images and URLs:
-echo -------------------------------
-
-for %%i in (%IMAGE_DIR%\*) do (
-    for %%x in (jpg jpeg png gif webp svg bmp) do (
-        echo %%~xi | find /i "%%x" >nul
-        if not errorlevel 1 (
-            echo !ESC![92m!BASE_URL!/%%~nxi%%~xi!ESC![0m
-        )
-    )
-)
-
-echo -------------------------------
-echo Done!
 pause
--- upload.bat
+++ upload.bat
@@ -23,18 +23,18 @@ git commit -m "%commitMsg%"
 git push
 
 echo.
+echo.
+echo.
+echo Upload is completed!
 echo Uploaded images and URLs:
 echo -------------------------------
-
 for %%i in (%IMAGE_DIR%\*) do (
     for %%x in (jpg jpeg png gif webp svg bmp) do (
         echo %%~xi | find /i "%%x" >nul
         if not errorlevel 1 (
-            echo !ESC![92m!BASE_URL!/%%~nxi%%~xi!ESC![0m
+            echo %ESC%[1;34m!BASE_URL!/%%~nxi%ESC%[0m
         )
     )
 )
-
 echo -------------------------------
-echo Done!
 pause

4d72cc0bde4e9d7d847206ff5a389e1addb86ad9
Pink Caravaggio <letongchen510@gmail.com>
2025-11-12 10:41:57 -0800

    Auto update 12-11-2025 10:41

--- update.bat
+++ update.bat
@@ -1,4 +1,15 @@
 @echo off
+setlocal EnableDelayedExpansion
+
+for /F %%a in ('echo prompt $E ^| cmd') do (
+    set "ESC=%%a"
+)
+
 cd /d "%~dp0"
+
 git pull origin main
+
+echo.
+echo %ESC%[32mDone!%ESC%[0m
+
 pause

21f3d37781ee16cda971c477a32b62e9599ce841
Pink Caravaggio <letongchen510@gmail.com>
2025-11-12 10:26:04 -0800

    Auto update 12-11-2025 10:26

--- test.bat
+++ test.bat
@@ -5,23 +5,6 @@ cd /d "%~dp0"
 
 set "BASE_URL=https://letongchen.art/cdn/images"
 set "IMAGE_DIR=images"
-
-git add -A
-
-for /f "tokens=1-4 delims=/- " %%a in ('date /t') do (
-    set day=%%a
-    set month=%%b
-    set year=%%c
-)
-for /f "tokens=1-2 delims=: " %%a in ('time /t') do (
-    set hour=%%a
-    set minute=%%b
-)
-
-set commitMsg=Auto update %year%-%month%-%day% %hour%:%minute%
-git commit -m "%commitMsg%"
-git push
-
 echo.
 echo Uploaded images and URLs:
 echo -------------------------------
